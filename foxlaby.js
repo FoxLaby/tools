87f6ed14fbf7ae16429f25c48fb6a2aafb5f2df0
Karim Mohamed <komicho1996@gmail.com>
2018-04-02 10:27:06 +0200

    add make ui app

--- foxlaby.js
+++ foxlaby.js
@@ -6,7 +6,7 @@ var path = require("path"),
     ncp = require('ncp').ncp;
 
 var allProcess = process.argv;
-var pathApp = __dirname + '\\app\\ngjs\\';
+var pathApp = __dirname + '\\app\\';
 var destination = path.resolve("./");
 
 var ngjs = function(destination_name = false)
@@ -14,7 +14,21 @@ var ngjs = function(destination_name = false)
     if ( destination_name ) {
         destination = destination + '\\' + destination_name;
     }
-    ncp(pathApp, destination, function (err) {
+    ncp(pathApp + 'ngjs\\', destination, function (err) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('Exiting files'.green); 
+    });
+}
+
+
+var UiApp = function(destination_name = false)
+{
+    if ( destination_name ) {
+        destination = destination + '\\' + destination_name;
+    }
+    ncp(pathApp + 'UiApp\\', destination, function (err) {
         if (err) {
             return console.error(err);
         }
@@ -26,4 +40,11 @@ if ( allProcess[2] == 'new' ) {
     if ( allProcess[3] == 'ngjs' ) {
         ngjs(allProcess[4]);
     }
+    if ( allProcess[3] == 'ui' ) {
+        UiApp(allProcess[4]);
+    } else {
+        console.log('We do not have such a thing in "new" !!!'.red); 
+    }
+} else {
+    console.log('You can use only "new" !!!'.red); 
 }
